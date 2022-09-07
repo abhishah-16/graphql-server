ff9d98cc47f2a8c6913dfaeff5a99155dd3e680d
abhishah <abhishah0961@gmail.com>
2022-09-07 17:09:07 +0530

    added: rootquery type

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const graphqlHTTP = require('express-graphql')
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema/schema')
 const app = express()
 
-// app.use('/graphql',graphqlHTTP({
-
-// }))
-app.listen(4000,() => {
+app.use('/graphql', graphqlHTTP({
+    schema
+}))
+app.listen(4000, () => {
     console.log('server is running on 4000');
 })
--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphQL = require('graphql')
-const { GraphQLObjectType, GraphQLString } = graphQL
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphQL
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -14,4 +14,25 @@ const BookType = new GraphQLObjectType({
             type: GraphQLString
         }
     })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parent, args) {
+
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
